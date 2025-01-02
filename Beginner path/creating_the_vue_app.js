858f50f07dd2f35ee7cc853b594813ccdfd82c55
Watson <bbcallyou2@gmail.com>
2025-01-02 19:17:41 +0800

    download intro to vue

---
+++ Beginner path/creating_the_vue_app.js
@@ -0,0 +1,9 @@
+//# 次處為options object 雖然教options 卻不能都是空值，至少要一個空物件
+//# 這樣會新增一個instance實例，而html裡面得id=app就會被它連接
+const app = Vue.createApp({
+    data() {
+        return {
+            product: "fff",
+        };
+    },
+});
