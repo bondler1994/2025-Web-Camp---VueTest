349426507233e1a18e09f7416b15189fcf39f125
Watson <bbcallyou2@gmail.com>
2025-01-09 19:41:40 +0800

    add @mouseover,and new methods

--- Beginner path/creating_the_vue_app.js
+++ Beginner path/creating_the_vue_app.js
@@ -3,16 +3,24 @@
 const app = Vue.createApp({
     data() {
         return {
+            cart: 0,
             product: "socks",
             image: "../Intro-to-Vue-3/assets/images/socks_green.jpg",
             // inStock: false,
             inventory: 0,
             details: ["50% happy", "30% jeremy", "20% watson"],
             variants: [
-                { id: 2235, color: "blue" },
-                { id: 2234, color: "blue" },
-                { id: 1, color: "black" },
+                { id: 2234, color: "green", image: "../Intro-to-Vue-3/assets/images/socks_green.jpg" },
+                { id: 2235, color: "blue", image: "../Intro-to-Vue-3/assets/images/socks_blue.jpg" },
             ],
         };
+    },
+    methods: {
+        addToCart() {
+            this.cart += 1;
+        },
+        updateImage(variantImage) {
+            this.image = variantImage;
+        },
     },
 });
