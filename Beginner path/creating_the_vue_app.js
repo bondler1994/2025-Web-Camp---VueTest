ebf4723e42798dab299bc8d12329fed335174bbc
Watson <bbcallyou2@gmail.com>
2025-01-13 11:22:50 +0800

    fix the photo disappear issue

--- Beginner path/creating_the_vue_app.js
+++ Beginner path/creating_the_vue_app.js
@@ -3,18 +3,21 @@
 const app = Vue.createApp({
     data() {
         return {
-            youtube: "https://www.youtube.com/watch?v=hWjwNgiLMgA&list=RDhWjwNgiLMgA&start_radio=1",
+            //test SZA youtube
+            // youtube: "https://www.youtube.com/watch?v=hWjwNgiLMgA&list=RDhWjwNgiLMgA&start_radio=1",
             cart: 0,
             testShow: false,
             product: "socks",
             brand: "Vue Mastery",
             selectedVariant: 0,
-            inStock: true,
             inventory: 0,
             details: ["50% happy", "30% jeremy", "20% watson"],
+            // tryIts: ["try1", "try2", "try3"],
             variants: [
                 { id: 2234, color: "green", image: "../Intro-to-Vue-3/assets/images/socks_green.jpg", quantity: 50 },
+                // , image: "../Intro-to-Vue-3/assets/images/socks_green.jpg", quantity: 50
                 { id: 2235, color: "blue", image: "../Intro-to-Vue-3/assets/images/socks_blue.jpg", quantity: 0 },
+                // image: "../Intro-to-Vue-3/assets/images/socks_blue.jpg", quantity: 0
             ],
         };
     },
@@ -23,12 +26,13 @@ const app = Vue.createApp({
             this.cart += 1;
         },
         updateVariant(index) {
-            this.variantImage = index;
-            console.log(index);
+            this.selectedVariant = index;
         },
     },
+    //用來計算用，相加相減
     computed: {
         title() {
+            //這裏連接到h1 相加了brand ＆ product
             return this.brand + " " + this.product;
         },
         image() {
