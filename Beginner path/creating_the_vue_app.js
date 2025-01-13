63d7bf926fb4e5073725b7790cd57619aeff7058
Watson <bbcallyou2@gmail.com>
2025-01-13 15:46:59 +0800

    bilud new js file and try template

--- Beginner path/creating_the_vue_app.js
+++ Beginner path/creating_the_vue_app.js
@@ -6,45 +6,8 @@ const app = Vue.createApp({
             //test SZA youtube
             // youtube: "https://www.youtube.com/watch?v=hWjwNgiLMgA&list=RDhWjwNgiLMgA&start_radio=1",
             cart: 0,
-            testShow: false,
-            brand: "Vue Mastery",
-            product: "socks",
-            h2test1: "you are",
-            h2test2: "fucking shit hole",
-            selectedVariant: 0,
-            inventory: 0,
-            details: ["50% happy", "30% jeremy", "20% watson"],
-            // tryIts: ["try1", "try2", "try3"],
-            variants: [
-                { id: 2234, color: "green", image: "../Intro-to-Vue-3/assets/images/socks_green.jpg", quantity: 50 },
-                // , image: "../Intro-to-Vue-3/assets/images/socks_green.jpg", quantity: 50
-                { id: 2235, color: "blue", image: "../Intro-to-Vue-3/assets/images/socks_blue.jpg", quantity: 0 },
-                // image: "../Intro-to-Vue-3/assets/images/socks_blue.jpg", quantity: 0
-            ],
         };
     },
-    methods: {
-        addToCart() {
-            this.cart += 1;
-        },
-        updateVariant(index) {
-            this.selectedVariant = index;
-        },
-    },
+    methods: {},
     //用來計算用，相加相減
-    computed: {
-        title() {
-            //這裏連接到h1 相加了brand ＆ product
-            return this.brand + " " + this.product;
-        },
-        image() {
-            return this.variants[this.selectedVariant].image;
-        },
-        inStock() {
-            return this.variants[this.selectedVariant].quantity;
-        },
-        h2test() {
-            return this.h2test1 + " " + this.h2test2;
-        },
-    },
 });
