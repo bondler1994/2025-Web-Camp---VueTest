36985844ad8aa86709e382def85b87afe4ce244e
Watson <bbcallyou2@gmail.com>
2025-01-23 18:42:57 +0800

    change prettier sitting

--- real-world-vue/src/components/EventCard.vue
+++ real-world-vue/src/components/EventCard.vue
@@ -1,14 +1,28 @@
 <template>
-    <div class="event-card"></div>
+  <div class="event-card">
+    <span>@ {{ event.time }} on {{ event.date }}</span>
+    <h4>{{ event.title }}</h4>
+  </div>
 </template>
 
 <script>
 export default {
-    name: "EventCard",
-    // props: {
-    //   msg: String,
-    // },
+  name: "EventCard",
+  // props: {
+  //   msg: String,
+  // },
+  data() {
+    return {};
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+.event-card {
+  padding: 20px;
+  width: 250px;
+  cursor: pointer;
+  border: 1px solid black;
+  margin-bottom: 10px;
+}
+</style>
